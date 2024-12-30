278da15dcc6fe72e0efd0dcd8420ceebacf6accc
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-12-30 15:57:28 +0530

    Backend Start

--- Farmisto-Backend/server.js
+++ Farmisto-Backend/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.send('Server is Working!');
+}
+);
+
 app.listen(process.env.port,()=>{
     console.log('Server is running');
 })
