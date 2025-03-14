f3cd59a9c694d69cded81ed6b223b29d5d1ec75c
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-03-14 21:58:45 +0530

    axios

--- Farmisto-Frontend/src/utils/axios.jsx
+++ Farmisto-Frontend/src/utils/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.createInstance({
+const instance = axios.create({
     baseURL: import.meta.NODE === 'development' ? 'http://localhost:4000' : '',
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`,
