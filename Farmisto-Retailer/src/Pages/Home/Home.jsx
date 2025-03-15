395473ef02af2e7cc20f5df9da9d6ad219b99b8f
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-03-15 17:18:13 +0530

    Axios Instance Setup

--- Farmisto-Retailer/src/Pages/Home/Home.jsx
+++ Farmisto-Retailer/src/Pages/Home/Home.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import NavBar from "../../Components/Major/Navbar";
+import NavBar from "../../Components/Major/NavBar";
 import Footer from "../../Components/Footer/Footer";
 import Homeheader from "../../Components/Header/Homeheader";
 import Panel from "../../Components/Panel/Panel";
-import Work from "../../Components/Work/Work";
+
 const Home = () => {
   return (
     <div className="relative h-screen">
       <NavBar />
       <Homeheader />
       <Panel />
-      {/* <div className="bg-[#d6e3bf]">
-        <Work />
-      </div> */}
       <Footer />
     </div>
   );
