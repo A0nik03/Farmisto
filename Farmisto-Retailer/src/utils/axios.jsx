cfb64bca21d5b336e727e65e48184cf6c8753aff
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-03-15 18:17:06 +0530

    Server Link

--- Farmisto-Retailer/src/utils/axios.jsx
+++ Farmisto-Retailer/src/utils/axios.jsx
@@ -3,10 +3,10 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const condition = import.meta.env.VITE_NODE;
 
-console.log("Server: ",condition === 'development' ? API_BASE_URL : 'https://farmisto.onrender.com',)
+console.log("Server: ",condition === 'development' ? API_BASE_URL : 'https://farmisto.onrender.com')
 
 const instance = axios.create({
-  baseURL: condition === 'development' ? API_BASE_URL : 'https://farmisto.onrender.com',
+  baseURL: "https://farmisto.onrender.com"
 });
 
 
